fa9e23cbdb9c3b38e902b550b1776435a3a0c7d8
martakov1 <63662599+martakov1@users.noreply.github.com>
2020-10-02 23:35:05 -0400

    week 1 hw

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,2 +1,6 @@
 import{gsap} from "gsap";
-gsap.to(".red-box",{duration: 2,alpha:0});
+var blueSpeed = 1;
+gsap.to(".red-box",{duration: blueSpeed,x:300});
+gsap.to(".blue-box",{duration: blueSpeed,x:300, delay: 1});
+gsap.to(".orange-box",{duration: blueSpeed,x:300, delay: 2});
+// gsap.to(".red-box",{duration: 2,alpha:0});
