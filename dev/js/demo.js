a430cc68985e1c7af0b1a1e7b82d59e7047208dd
martakov1 <63662599+martakov1@users.noreply.github.com>
2020-10-05 17:04:30 -0400

    push

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -2,6 +2,6 @@ import{gsap} from "gsap";
 var blueSpeed = 1;
 var demoTimeline = gsap.timeline();
 demoTimeline.to(".red-box",{duration: blueSpeed,x:300})
-.to(".blue-box",{duration: blueSpeed,x:300, delay: 1})
-.to(".orange-box",{duration: blueSpeed,x:300, delay: 2});
+demoTimeline.to(".blue-box",{duration: blueSpeed,x:300, delay: 1});
+demoTimeline.to(".orange-box",{duration: blueSpeed,x:300, delay: 2});
 // gsap.to(".red-box",{duration: 2,alpha:0});
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,12 +1,20 @@
 // import * as Demo from './demo.js';
 import{gsap} from "gsap";
-
+// import { Timeline } from "gsap/gsap-core";
 import {redBoxAnimation} from './redBox.js';
-console.log(Demo);
+import {blueBoxAnimation} from './blueBox.js';
+import {orangeBoxAnimation} from './orangeBox.js';
+// console.log(Demo);
+// function Demo(){
+//     var tl = gsap.timeline();
+//     console.log(tl);
+// }
+// demo();
+var mainTL = gsap "Timeline", ({paused:true});
 
-
-function Demo(){
-    var tl = gsap.timeline();
-    console.log(tl);
-}
-demo();
+mainTL.add(redBoxAnimation());
+     .add(blueBoxAnimation());
+     .addLabel("orange")
+     .add(orangeBoxAnimation());
+     .seek(orange);
+     .play();
